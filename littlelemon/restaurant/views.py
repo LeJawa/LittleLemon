0222f3200b64775fe085a592911faa42ab6d4416
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-10-02 11:45:15 +0200

    Add basic authentication

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer
@@ -11,6 +12,7 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated,]
 
 class SinlgeMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
