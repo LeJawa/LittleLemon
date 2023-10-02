fb49f675155e78c5fd545147cd04098a132688ad
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-10-02 11:06:07 +0200

    Implement basic API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.urls import path, re_path
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
+router = DefaultRouter()
+
+router.register(r'booking/tables', views.BookingViewSet)
+
+
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    re_path(r'^menu/?$', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SinlgeMenuItemView.as_view()),
 ]
+
+urlpatterns += router.urls
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SinlgeMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
